b60593b6c8aadce5225a6ca7cc727e121596086c
Kamil <kamil.nien@gmail.com>
2023-11-17 20:46:19 +0100

    Ditched the database solution.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Zasoby:
+Jeden z minusów Render:
+https://render.com/docs/free#spinning-down-on-idle
+
+Zasoby związane z developmentem:
 https://realpython.com/flask-google-login/
 
 https://developers.google.com/calendar/api/v3/reference/calendars/get?apix_params=%7B%22calendarId%22%3A%22primary%22%7D&%3Bhl=pl&hl=pl#python
@@ -8,3 +11,8 @@ https://developers.google.com/calendar/api/v3/reference/events?hl=pl#resource-re
 https://googleapis.github.io/google-api-python-client/docs/dyn/calendar_v3.html
 
 https://googleapis.github.io/google-api-python-client/docs/dyn/calendar_v3.events.html#insert
+
+Dostawca danych:
+https://sepan-wroclaw.alba.com.pl/harmonogram2023/
+
+https://sepan-wroclaw.alba.com.pl/files/eeb38e56-a54c-48cd-8c7c-6832c5822a6d/eeb38e56-a54c-48cd-8c7c-6832c5822a6d.html
--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 # Python standard libraries
 import json
 import os
-import sqlite3
 
 # Third-party libraries
 from dotenv import load_dotenv
 import httpx
-from flask import Flask, redirect, request, url_for
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
+from flask import Flask, redirect, request, session, url_for
 from oauthlib.oauth2 import WebApplicationClient
-
-# Internal imports
-from db import init_db_command
-from user import User
 
 # Configuration
 load_dotenv()
@@ -32,38 +20,20 @@ GOOGLE_DISCOVERY_URL = (
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY") or os.urandom(24)
 
-# User session management setup
-# https://flask-login.readthedocs.io/en/latest
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Naive database setup
-try:
-    init_db_command()
-except sqlite3.OperationalError:
-    # Assume it's already been created
-    pass
-
 # OAuth 2 client setup
 client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 
-# Flask-Login helper to retrieve a user from our db
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
-
-
 @app.route("/")
 def index():
-    if current_user.is_authenticated:
+    if 'name' in session:
         return (
-            f"<p>Hello, {current_user.name}! You're logged in!"
-            f"Email: {current_user.email}</p>"
+            f"<p>Hello, {session['name']}! You're logged in!"
+            f"<br/>Email: {session['email']}</p>"
             "<div><p>Google Profile Picture:</p>"
-            f'<img src="{current_user.profile_pic}" alt="Google profile pic">'
-            '</img></div>'
-            '<a class="button" href="/logout">Logout</a>'
+            f"<img src='{session['profile_pic']}' alt='Google profile pic'>"
+            "</img></div>"
+            "<a class='button' href='/logout'>Logout</a>"
         )
     else:
         return '<a class="button" href="/login">Google Login</a>'
@@ -106,7 +76,6 @@ def callback():
         redirect_url=request.base_url,
         code=code
     )
-    print(token_url, headers, body)
     token_response = httpx.post(
         token_url,
         headers=headers,
@@ -128,32 +97,25 @@ def callback():
     # The user authenticated with Google, authorized your
     # app, and now you've verified their email through Google!
     if userinfo_response.json().get("email_verified"):
-        unique_id = userinfo_response.json()["sub"]
-        users_email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
-        users_name = userinfo_response.json()["given_name"]
+        # uuid = userinfo_response.json()["sub"]
+        session['email'] = userinfo_response.json()["email"]
+        session['profile_pic'] = userinfo_response.json()["picture"]
+        session['name'] = userinfo_response.json()["given_name"]
     else:
         return "User email not available or not verified by Google.", 400
-
-    # Create a user in your db with the information provided
-    # by Google
-    user = User(
-        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
-    )
-
-    # Doesn't exist? Add it to the database.
-    if not User.get(unique_id):
-        User.create(unique_id, users_name, users_email, picture)
-
-    # Begin user session by logging the user in
-    login_user(user)
 
     # Send user back to homepage
     return redirect(url_for("index"))
 
 
 @app.route("/logout")
-@login_required
 def logout():
-    logout_user()
+    if 'name' in session:
+        session.pop('email', default=None)
+        session.pop('profile_pic', default=None)
+        session.pop('name', default=None)
     return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc")
